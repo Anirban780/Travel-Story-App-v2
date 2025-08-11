849f89701bcae1070289b8057976edfca537fc0a
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2025-08-11 19:55:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,25 @@ const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 const app = express();
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://travel-story-app-v2-1xil.vercel.app',
+  'https://travel-story-app-v2.vercel.app'
+];
+
+// Configure CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // your frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
 }));
+
 
 app.use(express.json());
 
